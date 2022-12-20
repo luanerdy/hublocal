776288880d80044d1c7559c2b4cf32ae5e95b590
luanerdy <luan.vini1@gmail.com>
2022-12-20 18:36:01 -0300

    feat: create responsavel modal

---
+++ src/components/CreateResponsavelForm/index.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable no-nonoctal-decimal-escape */
+import { Box, FormLabel, Text, Textarea, useToast } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import { Responsavel } from '../../@types/responsaveis'
+import { create } from '../../services/responsaveis'
+import { Input } from '../Input'
+import { SubmitButton } from '../SubmitButton'
+
+interface Props {
+	closeModal: () => void
+}
+
+export const CreateResponsavelForm = ({ closeModal }: Props) => {
+	const { register, handleSubmit } = useForm<Omit<Responsavel, 'id'>>()
+	const toast = useToast()
+
+	const onSubmit = async (data: Omit<Responsavel, 'id'>) => {
+		try {
+			await create({
+				...data,
+				telefone: data.telefone.replaceAll(/\D/gi, ''),
+			})
+			toast({
+				title: 'Sucesso',
+				description: 'Responsável criado com sucesso',
+				status: 'success',
+				duration: 5000,
+				isClosable: true,
+			})
+			closeModal()
+		} catch (err) {
+			toast({
+				title: 'Erro',
+				description: 'Não foi possível criar o responsável',
+				status: 'error',
+				duration: 5000,
+				isClosable: true,
+			})
+		}
+	}
+
+	return (
+		<Box as="form" overflowY="auto" onSubmit={handleSubmit(onSubmit)}>
+			<FormLabel m="0">
+				<Text fontWeight={500}>Nome:</Text>
+				<Input isRequired placeholder="Nome" {...register('nome')} />
+			</FormLabel>
+			<FormLabel m="0">
+				<Text fontWeight={500}>Telefone:</Text>
+				<Input
+					mask="(99) \9 9999-9999"
+					isRequired
+					placeholder="Telefone"
+					{...register('telefone')}
+				/>
+			</FormLabel>
+			<FormLabel m="0">
+				<Text fontWeight={500}>Endereço:</Text>
+				<Input
+					isRequired
+					placeholder="Endereço"
+					as={Textarea}
+					{...register('endereco')}
+				/>
+			</FormLabel>
+			<SubmitButton mb={5}>Criar</SubmitButton>
+		</Box>
+	)
+}
--- src/pages/dashboard/empresas/EmpresaForm/index.tsx
+++ src/pages/dashboard/empresas/EmpresaForm/index.tsx
@@ -9,10 +9,13 @@ import {
 	Textarea,
 	useToast,
 } from '@chakra-ui/react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useSelector } from 'react-redux'
 import { Empresa } from '../../../../@types/empresas'
+import { CreateResponsavelForm } from '../../../../components/CreateResponsavelForm'
 import { Input } from '../../../../components/Input'
+import { ModalInfo } from '../../../../components/ModalInfo'
 import { SubmitButton } from '../../../../components/SubmitButton'
 import { create } from '../../../../services/empresas'
 import { RootState } from '../../../../store/store'
@@ -22,6 +25,7 @@ interface Props {
 }
 
 export const EmpresaForm = ({ closeForm }: Props) => {
+	const [createResponsavel, setCreateResponsavel] = useState(false)
 	const { responsaveis } = useSelector((state: RootState) => state)
 	const { register, handleSubmit } = useForm<Omit<Empresa, 'id'>>()
 	const toast = useToast()
@@ -30,8 +34,8 @@ export const EmpresaForm = ({ closeForm }: Props) => {
 		try {
 			await create({
 				...data,
-				cnpj: data.cnpj.replaceAll(/\D/ig, ''),
-				responsavelPrincipal: Number(data.responsavelPrincipal)
+				cnpj: data.cnpj.replaceAll(/\D/gi, ''),
+				responsavelPrincipal: Number(data.responsavelPrincipal),
 			})
 			toast({
 				title: 'Sucesso',
@@ -41,8 +45,7 @@ export const EmpresaForm = ({ closeForm }: Props) => {
 				isClosable: true,
 			})
 			closeForm()
-		}
-		catch (err) {
+		} catch (err) {
 			toast({
 				title: 'Erro',
 				description: 'Ocorreu um erro',
@@ -54,73 +57,89 @@ export const EmpresaForm = ({ closeForm }: Props) => {
 	}
 
 	return (
-		<Flex
-			onSubmit={handleSubmit(onSubmit)}
-			as="form"
-			gap={10}
-			flexDir={{ base: 'column', md: 'row' }}
-			overflowY="auto"
-		>
-			<Box w="100%">
-				<Heading mb={10} size="md">
-					Adicionar Empresa
-				</Heading>
-				<FormLabel m="0">
-					<Text mb={5} fontWeight={500}>
-						Responsável Principal:
-					</Text>
+		<>
+			<ModalInfo
+				title="Criar Responsável"
+				isOpen={createResponsavel}
+				onClose={() => setCreateResponsavel(false)}
+			>
+				<CreateResponsavelForm
+					closeModal={() => setCreateResponsavel(false)}
+				/>
+			</ModalInfo>
+			<Flex
+				onSubmit={handleSubmit(onSubmit)}
+				as="form"
+				gap={10}
+				flexDir={{ base: 'column', md: 'row' }}
+				overflowY="auto"
+			>
+				<Box w="100%">
+					<Heading mb={10} size="md">
+						Adicionar Empresa
+					</Heading>
+					<FormLabel m="0">
+						<Text mb={5} fontWeight={500}>
+							Responsável Principal:
+						</Text>
 
-					<Select
-						bg="cyan.900"
-						color="white"
-						{...register('responsavelPrincipal')}
-					>
-						{responsaveis.map(({ id, nome }) => (
-							<option
-								style={{ background: '#065666' }}
-								value={id}
-								key={id}
-							>
-								{nome}
-							</option>
-						))}
-					</Select>
+						<Select
+							bg="cyan.900"
+							color="white"
+							{...register('responsavelPrincipal')}
+						>
+							{responsaveis.map(({ id, nome }) => (
+								<option
+									style={{ background: '#065666' }}
+									value={id}
+									key={id}
+								>
+									{nome}
+								</option>
+							))}
+						</Select>
 
-					<Button mt={5} bg="cyan.50" color="cyan.900">
-						Criar Responsável
-					</Button>
-				</FormLabel>
-			</Box>
-			<Box w="100%">
-				<FormLabel m="0">
-					<Text fontWeight={500}>Nome:</Text>
-					<Input
-						type='text'
-						isRequired
-						placeholder="Nome"
-						{...register('nome')}
-					/>
-				</FormLabel>
-				<FormLabel m="0">
-					<Text fontWeight={500}>CNPJ:</Text>
-					<Input
-						mask="99.999.999/9999-99"
-						isRequired
-						placeholder="CNPJ"
-						{...register('cnpj')}
-					/>
-				</FormLabel>
-				<FormLabel m="0">
-					<Text fontWeight={500}>Descrição:</Text>
-					<Input
-						isRequired
-						as={Textarea}
-						placeholder="Descrição"
-						{...register('descricao')}
-					/>
-				</FormLabel>
-				<SubmitButton mt={10}>Salvar</SubmitButton>
-			</Box>
-		</Flex>
+						<Button
+							mt={5}
+							bg="cyan.50"
+							color="cyan.900"
+							onClick={() => setCreateResponsavel(true)}
+						>
+							Criar Responsável
+						</Button>
+					</FormLabel>
+				</Box>
+				<Box w="100%">
+					<FormLabel m="0">
+						<Text fontWeight={500}>Nome:</Text>
+						<Input
+							type="text"
+							isRequired
+							placeholder="Nome"
+							{...register('nome')}
+						/>
+					</FormLabel>
+					<FormLabel m="0">
+						<Text fontWeight={500}>CNPJ:</Text>
+						<Input
+							mask="99.999.999/9999-99"
+							isRequired
+							placeholder="CNPJ"
+							{...register('cnpj')}
+						/>
+					</FormLabel>
+					<FormLabel m="0">
+						<Text fontWeight={500}>Descrição:</Text>
+						<Input
+							isRequired
+							as={Textarea}
+							placeholder="Descrição"
+							{...register('descricao')}
+						/>
+					</FormLabel>
+					<SubmitButton mt={10}>Salvar</SubmitButton>
+				</Box>
+			</Flex>
+		</>
 	)
 }
